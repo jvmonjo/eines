cbc4704edf74291c3c5c1543085130182f505b46
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-10-07 19:48:29 +0200

    REMOVED: unnecessary code

--- src/js/api/details.js
+++ src/js/api/details.js
@@ -29,32 +29,7 @@ export default function getDetails(slug) {
       placeOfDeath.innerHTML = details.placeOfDeath ? `in ${details.placeOfDeath}` : ''
       const death = document.getElementById('death')
       death.innerHTML = details.death ? details.death : ''
-      console.log(details);
-      // for (const member of members) {
-      //   const a = document.createElement('a');
-      //   a.href = `/house/${house}/${member.id}`;
-      //   const li = document.createElement('li');
-      //   const h3 = document.createElement('h3');
-      //   const gender = document.createElement('span');
-      //   gender.className = member.gender === 'male' ? 'fas fa-mars' : 'fas fa-venus';
-      //   const dead = document.createElement('span');
-      //   dead.className = member.alive ? 'alive' : 'fas fa-skull-crossbones';
-      //   h3.innerHTML = member.name;
-      //   a.appendChild(li);
-      //   li.appendChild(h3);
-      //   li.appendChild(dead);
-      //   li.appendChild(gender);
-      //   li.id = member.id;
-      //   li.className = 'card';
-      //   toAdd.appendChild(a);
-      //   document.getElementById('members').appendChild(toAdd);
-      //   console.log(member);
-      // }
-      // const div = document.createElement("div")
-      // const name = document.getElementById('name')
-      // name.innerHTML = response.data.quote
-      // const gender = document.getElementById('gender')
-      // gender.innerHTML = response.data.character
+      // console.log(details);
     })
     .catch(function (error) {
       console.log(error);
