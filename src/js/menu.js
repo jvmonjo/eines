e708a8f9c959e48e09f0c9d258478aa9ad61ba7e
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-10-08 11:57:03 +0200

    REMOVED: log

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,7 @@
 export default function handleMenu() {
   const btn = document.getElementById('menu-btn')
   const nav = document.getElementById('nav-menu')
-  console.log('btn', btn)
+  // console.log('btn', btn)
   if (btn !== null) {
     btn.addEventListener("click", () => {
       btn.classList.toggle("is-active")
