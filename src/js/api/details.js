08d5ef6c686c1ade35b7f3b17149ffa8a1795138
Josep Vicent Monjo i Agut <josep@MacBook-Air-de-Josep.local>
2019-10-14 10:28:37 +0200

    ADDED: related members

--- src/js/api/details.js
+++ src/js/api/details.js
@@ -33,8 +33,40 @@ export default function getDetails(slug) {
       const death = document.getElementById('death')
       death.innerHTML = details.death ? details.death : ''
       // console.log(details);
+      // Populate related
+      return axios.get(`https://api.got.show/api/book/characters/byHouse/${details.house}`)
+        .then(function (response) {
+          const members = response.data
+          const relatedMembers = members.slice(0, 4)
+          const relatedContainer = document.getElementById('members')
+          const toAdd = document.createDocumentFragment();
+          for (const member of relatedMembers) {
+            const a = document.createElement('a');
+            a.href = `/details/?slug=${member.slug}`;
+            const li = document.createElement('li');
+            const h3 = document.createElement('h3');
+            const gender = document.createElement('span');
+            gender.className = member.gender === 'male' ? 'fas fa-mars' : 'fas fa-venus';
+            const dead = document.createElement('span');
+            dead.className = member.alive ? 'alive' : 'fas fa-skull-crossbones';
+            h3.innerHTML = member.name;
+            a.appendChild(li);
+            li.appendChild(h3);
+            li.appendChild(dead);
+            li.appendChild(gender);
+            li.id = member.id;
+            li.className = 'card';
+            toAdd.appendChild(a);
+            relatedContainer.appendChild(toAdd);
+            // console.log(member);
+          }
+        })
     })
     .catch(function (error) {
       console.log(error);
     })
+}
+
+export function getRelated() {
+
 }
